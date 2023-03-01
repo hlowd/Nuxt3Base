31bc14f54dcdfcbcaa9e14e7af7238343df91e83
zf <tosee@yeah.com>
2023-03-01 09:54:26 +0800

    123

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
 
+export default defineNuxtConfig({
+    modules: [
+        '@pinia/nuxt',
+        '@pinia-plugin-persistedstate/nuxt',
+      ],
+      piniaPersistedstate: {
+        cookieOptions: {
+          sameSite: 'strict',
+        },
+        storage: 'localStorage',
+      },
 })
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup>
+import {useUserStore } from "~/store/userStore"
+const s = useUserStore();
+</script>
+
+<template>
+    <div>
+        {{s.name}}
+    </div>
+</template>
